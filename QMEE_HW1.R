b82cf8deafd18705256b66f1a0720e663b3b41cd
noah-mt-smith <nmts9971@gmail.com>
2024-01-12 00:42:20 -0500

    HW1 update 5

--- QMEE_HW1.R
+++ QMEE_HW1.R
@@ -8,8 +8,8 @@
 ## See "README.md" for an explanation of how the dataset was collected, 
 ## the context of the experiment, and our main explicit predictions. 
 
-# Our prediction was that participants would allocate significantly more 
-# funds and coaching hours to winners than to losers. We can statistically 
+# Our predictions were that participants would allocate significantly more 
+# 1) funds and 2) coaching hours to winners than to losers. We can statistically 
 # test these predictions with two one-sample t-tests with mu = 0.5
 # (for both proportion of money allocated to winner and proportion of coaching
 # hours allocated to winner).
