04c0363f9f0e5092c86afafcbebc7a73e9fb9ea4
noah-mt-smith <nmts9971@gmail.com>
2024-01-11 23:53:33 -0500

    HW1

---
+++ Smith_QMEE_HW1.R
@@ -0,0 +1,14 @@
+#Noah Smith QMEE HW1
+
+# See "QMEE_WL_social_data.csv" for the dataset used to conduct this one-sample
+# t-test.
+
+# Also, see "README.md" for an explanation of how the dataset was collected, 
+# the context of the experiment, and our main explicit predictions. 
+
+# Our prediction was that participants would allocate significantly more 
+# funds and coaching hours to winners than to losers. We can statistically 
+# test these predictions with two one-sample t-tests with mu = 0.5
+# (for both proportion of money allocated to winner and proportion of coaching
+# hours allocated to winner).
+
