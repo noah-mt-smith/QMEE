9ec73bd75d74b274877cea0b45f217899506ebc4
noah-mt-smith <nmts9971@gmail.com>
2024-01-12 10:28:32 -0500

    HW1 update 6

--- QMEE_HW1.R
+++ QMEE_HW1.R
@@ -27,7 +27,7 @@ prop.money.t.test <- t.test(prop.money.to.winner, mu = 0.5, alternative = "two.s
 
 print(prop.money.t.test)
 
-# We reject the null that participants allocate 0.5 of their money to the winner, and our t-statistic is positive, 
+# Since p < 0.05, we reject the null that participants allocate 0.5 of their money to the winner, and our t-statistic is positive, 
 # which indicates that participants allocated significantly more than 0.5 of their money to the winner.
 
 # One-sample t-test with the null that participants allocate 0.5 of their coaching hours to the winner
@@ -38,5 +38,5 @@ prop.coaching.t.test <- t.test(prop.coaching.to.winner, mu = 0.5, alternative = 
 
 print(prop.coaching.t.test)
 
-# We reject the null that participants allocate 0.5 of their coaching hours to the winner, and our t-statistic is negative, 
+# Since p < 0.05, we reject the null that participants allocate 0.5 of their coaching hours to the winner, and our t-statistic is negative, 
 # which indicates that participants allocated significantly less than 0.5 of their coaching hours to the winner.
