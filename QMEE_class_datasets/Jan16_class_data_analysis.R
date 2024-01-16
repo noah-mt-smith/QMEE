1b9f65171c0e0c9083b9cf957406a61ad19fd826
noah-mt-smith <nmts9971@gmail.com>
2024-01-16 12:03:34 -0500

    update to jan 16 notes

--- QMEE_class_datasets/Jan16_class_data_analysis.R
+++ QMEE_class_datasets/Jan16_class_data_analysis.R
@@ -1,1 +1,11 @@
 # Jan 16 class dataset analysis
+
+getwd()
+setwd("/Users/noahsmith/QMEE/QMEE_class_datasets")
+download.file(url="https://ndownloader.figshare.com/files/2292169",
+              destfile = "/Users/noahsmith/QMEE/QMEE_class_datasets/Jan16_class_dataset.csv")
+
+# need to update R -- # see chapter 5 of happygitwithr
+
+Jan16data = read_csv("Jan16_class_data.csv")
+View(Jan16data)
