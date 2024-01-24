a8ca8ef0ad1212b8d0a60600b43f103d46638f93
noah-mt-smith <nmts9971@gmail.com>
2024-01-24 15:26:37 -0500

    QMEE HW3

---
+++ QMEE_HW3.R
@@ -0,0 +1,11 @@
+# Noah Smith QMEE HW3
+
+WLdata = read.csv("QMEE_WL_social_data.csv")
+
+View(WLdata)
+# View(WLdata)
+
+# create the boxplot dataframes (cis only) using bolker's suggested code on the
+# previous assignment.
+
+
