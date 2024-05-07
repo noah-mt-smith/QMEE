119843f4580f1f5cfc567b2d36496c1da46ec7c0
Ben Bolker <bbolker@gmail.com>
2024-05-06 20:19:07 -0400

    BMB comments on HW8

--- QMEE_HW8.R
+++ QMEE_HW8.R
@@ -44,9 +44,14 @@ b_prior <- c(set_prior("normal(0.5,0.02)", "Intercept"), # intercept at 0.5, sma
 # out after several hours and many visits to the stan forums, so I 
 # came back to these priors instead.
 
+## BMB: fair enough.
+
 # With these priors, I'm beginning with the assumption that people don't 
 # have any bias in giving more or less money to winners. I'm also 
-# assuming that there isn't a ton of variation in these preferences. 
+## assuming that there isn't a ton of variation in these preferences.
+
+## BMB: I would probably be careful not to be *too* opinionated in my priors ...
+## (i.e. don't make them too narrow, just rule out unrealistic outcomes)
 
 make_stancode(prop.allocated ~ resource + participant.gender + (1|participant.id), 
               data = WLdata_long, prior = b_prior)
@@ -64,7 +69,8 @@ test_prior <- function(p) {
              silent = 2, refresh = 0)
   )
   p_df <- WLdata_long |> add_predicted_draws(b)
-  gg1 <- ggplot(p_df, aes(resource, .prediction)) + geom_jitter() + ylim(0,1) + labs(x = "Resource allocated", 
+  gg1 <- ggplot(p_df, aes(resource, .prediction)) + geom_jitter() +
+      geom_violin(fill = "red", alpha = 0.5) + ylim(0,1) + labs(x = "Resource allocated", 
   y = "Proportion of resource allocated to winner (simulated prediction)")
   print(gg1)
   invisible(b)
@@ -97,12 +103,16 @@ b_custom1 <- brm(form1, WLdata_long, prior = b_prior, seed = 15,
 # quite sure how to fix this. I think this might have to do with the small size 
 # of my priors, but I'm not exactly sure.
 
+## BMB: hmm, it's not unhappy for me (quick, no reports of divergence) ?
 print(bayestestR::diagnostic_posterior(b_custom1),
       digits = 4)
 
 # the values here all look good to me, but I'm not sure about the tiny
 # MCSE values. They do look very small, but I'm not sure if this suggests
 # that something is wrong with my model, I.e., deflating these values.
+
+## BMB: I think that's fine. We are estimating the uncertainty in the mean of
+## the *sample*, not of the data ...
 
 mcmc_trace(b_custom1, regex_pars = "b_|sd_")
 
@@ -172,12 +182,16 @@ ggplot(res, aes(estimate, term, colour = model, shape = model)) +
 
 # We can also do some posterior predictive simulations of my data
 
-posterior_df <- WLdata_long |> add_predicted_draws(b_custom1)
+## sample fewer draws (for speed - can always go back and remake plots with full sample)
+posterior_df <- WLdata_long |> add_predicted_draws(b_custom1, ndraws = 100)
 
-gg2 <- ggplot(posterior_df, aes(resource, .prediction, group = interaction(participant.id, .draw))) +
-  geom_jitter() + labs(y = "Proportion allocated to winner") + geom_point(aes(y = prop.allocated), col = "skyblue")
+gg2 <- ggplot(posterior_df, aes(resource, .prediction)) +
+    geom_jitter(alpha=0.5) + labs(y = "Proportion allocated to winner") +
+    stat_sum(data = WLdata_long, aes(y = prop.allocated), col = "skyblue")
 
 print(gg2)
+
+## BMB: not quite sure how we ended up with more than 1e6 samples ...
 
 # This shows the posterior predicted distributions, however they are slightly odd because they are so wide
 # and include values above and below zero. I have a feeling they are so wide because the distribution of observed
@@ -186,8 +200,13 @@ print(gg2)
 # mitigated if I'd used a beta distributed response, but as I mentioned earlier, I tried that approach 
 # for a good amount of time and couldn't get past properly setting the priors.
 
-gg_violin <- ggplot(WLdata_long, aes(x = resource, y = prop.allocated)) + geom_violin()
+## BMB: OK.
+
+gg_violin <- ggplot(posterior_df, aes(x = resource, y = .prediction)) + geom_violin(fill = "gray") +
+    stat_sum(data = WLdata_long, aes(y = prop.allocated), col = "blue")
 print(gg_violin)
+
+## BMB: looks like prior might not be capturing the lower tail of money allocation?
 
 # gg3 <- ggplot(posterior_df, aes(resource, .prediction, group = interaction(participant.id, .draw))) 
 # + geom_jitter() + labs(y = "Proportion allocated to winner") 
@@ -204,3 +223,5 @@ print(gg_violin)
 # all (see "gg_violin"). Although I tried to overlay the violin plot, it still wouldn't work even after
 # a couple hours worth of (attempted) debugging. I'm not sure if this has something to do with the stan
 # package or the way that the code puts the plot together, but I would've much preferred such a plot. 
+
+## BMB: overall this is good. If you want to work through priors for Beta analyses sometime, let me know.
